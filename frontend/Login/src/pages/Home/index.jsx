fdc17288f1301054d43915328d400671609bbc01
Israel vitor <vitor1914@gmail.com>
2022-12-18 09:51:08 -0300

    12/12

--- frontend/Login/src/pages/Home/index.jsx
+++ frontend/Login/src/pages/Home/index.jsx
@@ -1,7 +1,38 @@
-function Home(){
-    <div>
-        <h1>Home</h1>
-    </div>
+import amazim from "../../assets/amazim.png";
+import '../Home/style.css'
+
+function Home() {
+    return (
+
+        <div className="Tela-inicio">
+            <h1>Tela inicial Amazonas</h1>
+            <input type="Buscar" />
+            <button>Buscar</button>
+
+            <div className="logo">
+                <img src={amazim} alt="" />
+            </div>
+            <div className="Tela-principal">
+                <button>Pagina inicial</button>
+            </div>
+            <div class="menu">
+                <span class="menu-trigger"></span>
+                <ul class="menu-menu">
+                    <li>usuario1</li>
+                    <li>usuario2</li>
+                    <li>usuario3</li>
+                </ul>
+                
+            </div>
+        </div>
+
+
+
+
+
+    )
+
 }
 
 export default Home;
+
